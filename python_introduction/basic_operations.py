cdf544a5b229a20b2d1f7675c2ba5dbfe685e42f
MarwanIssa100 <missa4312@gmail.com>
2024-09-12 22:49:04 +0300

    add Basic Arithmetic Exercise code file

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+add = number1 + number2
+diff = number1 - number2
+product = number1 * number2
+
+print("Addition of " ,number1, " and ", number2 ," is ", add)
+print("Subtraction of ", number1 ," and ", number2 ," is ", diff)
+print("Multiplication of " ,number1 ," and " ,number2, " is " ,product)
