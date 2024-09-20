d2c21a21cf54cfefa06a2db4976b95921e261fd8
MarwanIssa100 <missa4312@gmail.com>
2024-09-20 17:39:45 +0300

    add daily reminder script

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,16 @@
+task = input("Enter the task description: ")
+priority = input("Enter the priority (high , medium , low): ")
+time = input("Is it time-bound? (yes/no): ")
+
+match priority:
+    case "high":
+        print(f"Reminder: {task} is a {priority} priority task")
+    case "medium":
+        print(f"Reminder: {task} is a {priority} priority task")
+    case "low":
+        print(f"Note: {task} is a {priority} priority task.")
+
+if time == "yes":
+    print("that requires immediate attention today!")
+else:
+    print("Consider completing it when you have free time.")
---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,7 @@
+size = int(input("Enter the size of the pattern: "))
+while size > 0 :
+    for i in range(0, size-1):
+        print("*", end = "")
+    print("*")
+
+        
