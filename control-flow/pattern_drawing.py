3496b1dc33c7282d9de96ab47bc9aae5227dff51
MarwanIssa100 <missa4312@gmail.com>
2024-09-20 17:50:41 +0300

    edit in pattern_drawing script

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,7 +1,9 @@
 size = int(input("Enter the size of the pattern: "))
-while size > 0 :
+limit = size
+while limit > 0 :
     for i in range(0, size-1):
         print("*", end = "")
     print("*")
+    limit -= 1
 
         
