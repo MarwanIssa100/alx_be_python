8034d08c8378526df3d9bb0ccb77bf6ce91ab830
MarwanIssa100 <missa4312@gmail.com>
2024-09-28 05:42:47 +0300

    edit in time the script

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,14 +1,14 @@
-from datetime import datetime
+import datetime
+
 def display_current_datetime():
     current_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     return current_date
 
-daysNumber = input("Enter the number of days to add to the current date: ")
 
-def calculate_future_date():
-    future_date = datetime("%Y-%m-%d %H:%M:%S", daysNumber)
-    print(future_date)
-    # future_date = display_current_datetime() + future_date
-    return future_date
+def calculate_future_date(days_number):
+    future_date = datetime.datetime.now() + datetime.timedelta(days=int(days_number))
+    return future_date.strftime("%Y-%m-%d %H:%M:%S")
 
-print("Future date:",calculate_future_date())
+
+days_number = input("Enter the number of days to add to the current date: ")
+print("Future date:", calculate_future_date(days_number))
--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,16 +1,16 @@
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9 
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
-temperature = float(input("Enter the temperature: "))
+temperature = float(input("Enter the temperature to convert: "))
 
 tempKind = input("Is this temperature in Celsius or Fahrenheit? (C/F):").upper()
 
 def convert_to_celsius(fahrenheit):
-        fahrenheit = temperature * FAHRENHEIT_TO_CELSIUS_FACTOR + 32
+        fahrenheit = (temperature - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR 
         print(temperature,"°C is",fahrenheit)
 
 def convert_to_fahrenheit(celsius):
-        celsius = temperature * CELSIUS_TO_FAHRENHEIT_FACTOR + 32
+        celsius = (temperature * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
         print(temperature,"°F is",celsius)
 
 if tempKind == "C":
@@ -18,4 +18,4 @@ if tempKind == "C":
 elif tempKind == "F":    
     convert_to_fahrenheit(temperature)
 else:
-    print("Invalid input")
+    print("Invalid temperature. Please enter a numeric value.")
