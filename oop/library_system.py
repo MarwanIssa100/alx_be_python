d87fc409958c9d5d49750cdfe30aae121d950a51
Marwan_Issa <missa4312@gmail.com>
2024-10-13 01:13:50 +0300

    add Mastering Inheritance and Composition in Python task

---
+++ oop/library_system.py
@@ -0,0 +1,30 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+    def __str__(self):
+        return f"Book: {self.title} by {self.author}"
+class EBook(Book):
+    def __init__(self, title, author, file_format):
+        super().__init__(title, author)
+        self.file_format = file_format
+    def __str__(self):
+        return super().__str__() + f", File format: {self.file_format}"
+
+class PrintBook(Book):
+    def __init__(self, title, author, page_count):
+        super().__init__(title, author)
+        self.page_count = page_count
+    def __str__(self):
+        return super().__str__() + f", Page count: {self.page_count}"
+
+
+class Library:
+    def __init__(self):
+        self.books = []
+    def add_book(self, book):
+        self.books.append(book)
+
+    def list_books(self):
+        for book in self.books:
+            print(book)
